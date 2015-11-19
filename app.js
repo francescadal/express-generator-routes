1c13aa78b802d380c3bd15f5a297f6e040668ef5
Jeffrey Horn <jeffh@generalassemb.ly>
2015-11-19 12:24:06 -0500

    Add solution for time.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var hello = require('./routes/hello');
+var time = require('./routes/time');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/hello', hello);
+app.use('/time', time);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
